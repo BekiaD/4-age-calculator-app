14df5f9a4ea8d64e00ad18fb64b5e5be3b5e60c1
BekiaD <144695091+BekiaD@users.noreply.github.com>
2024-02-26 21:12:57 +0100

    omg
    finally done

--- index.js
+++ index.js
@@ -42,34 +42,73 @@ button.addEventListener('click', function(e){
     const pBYear        = birthYear.toString()
     const pBMonth       = birthMonth.toString().padStart(2,"0")
     const pBDay         = birthDay.toString().padStart(2,"0")
-    const birthDatePad  = `${pBYear}-${pBMonth}-${pBDay}`
+    const birthDatePad  = `${pBMonth}/${pBDay}/${pBYear}`
     // console.log(birthDatePad)
 
-    const isValidDate = checkValidDate(birthDay, birthMonth) 
-    // if (isValidDate){
-    //     console.log('trutru')
-    // } else {
-    //     console.log('please good')
-    // }
+    const isValidDate   = checkValidDate(birthDay, birthMonth) 
 
     if(!isValidDate){
-        alert('Please add your birthdate')
+        alert('Please add a valid date in format: DD/MM/YYYY')
         return
     } 
-    console.log('trutru')
 
-    console.log('useful message')
-    // function to calculate age
+    const differenceInDays    = calculateDays(birthDatePad)
 
-    const birthdayInDays = 
+    const actualDifferenceArr = convertDays(differenceInDays)
+    
+    let outputDays   = actualDifferenceArr[2]
+    let outputMonths = actualDifferenceArr[1]
+    let outputYears  = actualDifferenceArr[0]
 
-    // 
+    console.log(outputDays, outputMonths, outputYears)
+
+    //print age
+    yearsSpan.textContent = outputYears
+    monthsSpan.textContent = outputMonths
+    daysSpan.textContent = outputDays
+
 })
 
+function convertDays (totalDays){
+    let years    = 0
+    let months   = 0
+    let daysLeft = totalDays     // let days = 0  DAYS
+
+    while (daysLeft >= 365){
+        years++
+        daysLeft = daysLeft -365  // daysleft -= 365
+    } 
+    // console.log('YEARS', years, daysleft)
+
+    while (daysLeft >= 30){
+        months++
+        daysLeft = daysLeft - 30
+    }
+    // console.log('MONTHS', years, months, daysLeft)
+
+    return [years, months, daysLeft]
+
+}
+
+// function to calculate age
+function calculateDays(birthDate){
+    let bday = new Date(birthDate) //console.log(bday)
+    let today = new Date()
+
+    // Calculating the time difference of two dates
+    let differenceInTime = today.getTime() - bday.getTime()
+
+    // Calculating the no. of days between two dates
+    let differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24))-1
+
+    // To display the final number of days (result)
+    // console.log(`Total number of days: ${differenceInDays}`)
+    return differenceInDays
+  }
 
 
-  // check if birth date exists - number of days/months
-  function checkValidDate(day, month){
+// check if birth date exists - number of days/months
+function checkValidDate(day, month){
     // console.log(day, month)
     if (
         day > 31 ||
